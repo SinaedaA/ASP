34712c058a29d4cbcd2ba64061c0318839146495
Sinaeda Anderssen <sinaeda@Sinaedas-MacBook-Pro.local>
2023-08-31 14:32:14 +0200

    filter filtered test

--- scripts/dada2_functions.R
+++ scripts/dada2_functions.R
@@ -61,8 +61,8 @@ dada2_wrap <- function(inpath, filenames = list(), maxEE, truncQ, truncLen, trim
         print(filt_df)
         r1s <- row.names(filt_df)
         r2s <- sub("R1", "R2", row.names(filt_df))
-        print(filtered_F)
-        print(r1s)
+        filtered_F <- file.path(inpath, "filtered", r1s)
+        filtered_R <- file.path(inpath, "filtered", r2s)
         ## Learning error rates
         errors_F <- learnErrors(filtered_F, multithread = TRUE)
         errors_R <- learnErrors(filtered_R, multithread = TRUE)

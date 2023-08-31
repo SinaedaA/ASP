eacd56a79c8ab6e76359d221b565d4b9d246e947
Sinaeda Anderssen <sinaeda@Sinaedas-MacBook-Pro.local>
2023-08-31 13:17:00 +0200

    filter filtered test

--- scripts/dada2_functions.R
+++ scripts/dada2_functions.R
@@ -57,8 +57,8 @@ dada2_wrap <- function(inpath, filenames = list(), maxEE, truncQ, truncLen, trim
         )
         print("filtered table")
         print(filtered)
-        print(typeof(as.data.frame(filtered)))
-        print(filtered[filtered$reads.out > 1000,])
+        filt_df <- as.data.frame(filtered)
+        print(filt_df[filt_df$reads.out > 1000,])
         ## Learning error rates
         errors_F <- learnErrors(filtered_F, multithread = TRUE)
         errors_R <- learnErrors(filtered_R, multithread = TRUE)
